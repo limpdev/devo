ffa0f72abfc1c0b6024baccc3d2cd2a9735e1881
limpdev <drewgorbet2020@gmail.com>
2025-05-12 20:22:34 -0500

    Updating...

--- bookParser.go
+++ bookParser.go
@@ -10,6 +10,22 @@ import (
 	"strings"
 )
 
+// TOCItem represents an item in the Table of Contents
+type TOCItem struct {
+	Title    string    `json:"title"`
+	Path     string    `json:"path,omitempty"` // Relative path to the .md file from ./book/src/
+	Level    int       `json:"level"`          // Indentation level
+	Children []TOCItem `json:"children,omitempty"`
+}
+
+// BookData holds the TOC and the content of the initially loaded chapter
+type BookData struct {
+	TOC             []TOCItem `json:"toc"`
+	InitialMarkdown string    `json:"initialMarkdown"`
+	InitialPath     string    `json:"initialPath"`     // Path of the initially loaded markdown
+	Error           string    `json:"error,omitempty"` // In case of loading errors
+}
+
 // ... (App struct, NewApp, startup, shutdown, Greet, GetMarkdownContent from previous steps)
 
 const bookSrcPath = "./book/LimpBook/" // Define base path for book source
